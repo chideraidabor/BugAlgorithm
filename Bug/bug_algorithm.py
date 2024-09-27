a26e85ce50cc132f5adf272d0a62a645398a81a8
chidera idabor <idabor001@gannon.edu>
2024-09-26 21:18:32 -0400

    created shapes in grid using rectangles and l shapes made from rectangles

--- Bug/bug_algorithm.py
+++ Bug/bug_algorithm.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 #1. get the robot from the goal to start via the calculated m line
 #2. combine bug 1 and 2
 #3. bug 1 - The robot heads straight towards the goal until it hits an obstacle, then explores the whole obstacle, then finds the shortest path from the obstacle to the goal.
@@ -12,24 +13,46 @@ import numpy as np
 #10. bug 2 will check if it crosses the mline then it will move foward to the goal
 #11. end if reaches the goal or cant find a way to goal
 
-def create_environment(rows, cols, obstacles):
+start = (0,0)
+goal = (4,4)
+rectangle_obstacles = [(1,1,3,1), # rec 1 - position 1,1 with width of 3 and height of 1
+                       (4,4,2,2)] # rec 2 - position 3,3 with width of 2 and height of 2
+l_obstacles = [(2, 2, 1, 3), #circle 1 - center position at 2,2 with a radius of 1
+               (2, 2, 3, 1)] #circle 2 - center position at 4,1 with a radius of 2
+
+def create_environment(rows, cols, rectangle_obstacles, l_obstacles):
     """
     param rows: number of rows in grid
     param cols: number of columns in grid
-    param obstacles: number is obstacles in grid. tuple showing the position of tuple
+    param rectangle_obstacles: tuple showing the position of the rectangle
     return: the 2d grid with ostacles placed
     """
     grid = np.zeros((rows,cols)) #new grid based on num of rows and col 0 meaning free space
 
-    for obstacle in obstacles:
-        grid[obstacle[0], obstacle[1]] = 1
-    
+    for rect in rectangle_obstacles:
+        top_left_row, top_left_col, width, height = rect
+        for i in range(top_left_row, top_left_row + height):
+            for j in range(top_left_col, top_left_col + width):
+                if i < rows and j < cols:
+                    grid[i,j] = 1
+
+    for l_shape in l_obstacles:
+        top_left_row, top_left_col, width, height = l_shape
+        for i in range(top_left_row, top_left_row + height):
+            for j in range(top_left_col, top_left_col + width):
+                if i < rows and j < cols:
+                    grid[i, j] = 1
+
     return grid
 
-#obstacles = [(1, 1), (2, 1), (3, 1), (1, 3), (2, 3), (3, 3)]
-#print(create_environment(5,5,obstacles))
+def visualize_env(grid):
+    plt.imshow(grid,cmap="gray_r")
+    plt.title("Environment")
+    plt.grid(True)
+    plt.show()
 
-        
+#create_environment(6, 6, rectangle_obstacles, circle_obstacles) 
+visualize_env(create_environment(6,6,rectangle_obstacles, l_obstacles))       
 
 
 
